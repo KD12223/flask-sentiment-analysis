38d70d0ca4a591ee3b69765f62cad4685a958bc3
Kyler Deggs <deggskyler@icloud.com>
2021-03-13 19:47:38 -0600

    Added support for sentiment analysis

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+import boto3 as boto3
+from flask import Flask, jsonify, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
+comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+# Determines the sentiment of text and returns the results
+@app.route('/analyze', methods=['POST'])
+def analyze_text():
+    body = request.get_json()
+    text = body.get('content')
+
+    if text:
+        return jsonify(comprehend.detect_sentiment(Text=text, LanguageCode='en'))
+    else:
+        raise BadRequest('The content is missing from the request')
+
+
+# Handles all exceptions
+@app.errorhandler(Exception)
+def exception_handler(e):
+    return jsonify(e.description), e.code
 
 
 if __name__ == '__main__':
